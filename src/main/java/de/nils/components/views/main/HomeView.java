45f777e7fb4ee017f49d6e6e9aad0dff9e2fdb64
Nils <Nils-Arne.Abbing@adesso.de>
2021-05-17 12:30:01 +0200

    init setup

---
+++ src/main/java/de/nils/components/views/main/HomeView.java
@@ -0,0 +1,9 @@
+package de.nils.components.views.main;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class HomeView extends VerticalLayout {
+    public HomeView(){
+
+    }
+}
